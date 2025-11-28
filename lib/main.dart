76a23c825c074c112dcead6117d68b9131fc5ed8
Luísa Coelho <luisacoutinhocoelho@gmail.com>
2025-11-28 13:28:54 -0300

    feat: use supabase code generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,126 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:provider/provider.dart';
+import 'supabase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:geo_masters/providers/auth_provider.dart';
+import 'package:geo_masters/providers/theme.provider.dart';
+import 'package:geo_masters/screens/open.dart';
 
-import 'providers/theme.provider.dart';
-import 'providers/highest_score.provider.dart';
-import 'providers/highest_streak.provider.dart';
-import 'screens/open.dart';
+// Change to true to enable authentication
+// with Google Sign In
+const authenticationEnabled = false;
 
-List<dynamic> data = [];
-
+// Sample supbase_options.dart file:
+//
+// const Map<Symbol, dynamic> supabaseOptions = {
+//   #url: 'https://qwerty.supabase.co',
+//   #anonKey: '1234567890',
+// };
 void main() async {
-  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
 
-  await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
-  );
+  await dotenv.load(fileName: '.env');
+  await Function.apply(Supabase.initialize, [], supabaseOptions);
 
-  data = await Supabase.instance.client.from('countries').select();
-
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ChangeNotifierProvider(create: (_) => HighestScoreProvider()),
-        ChangeNotifierProvider(create: (_) => HighestStreakProvider()),
-      ],
-      child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, _) {
-          return MaterialApp(
-            title: 'Geo Masters',
-            themeMode: themeProvider.themeMode,
-            theme: ThemeData(
-              brightness: Brightness.light,
-              primaryColor: Colors.blue[200],
-              outlinedButtonTheme: OutlinedButtonThemeData(
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Colors.black),
-                  fixedSize: Size(200, 50),
-                ),
-              ),
-              textTheme: TextTheme(
-                bodyLarge: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                bodyMedium: TextStyle(color: Colors.black, fontSize: 20),
-                bodySmall: TextStyle(color: Colors.black),
-              ),
-            ),
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-              primaryColor: const Color.fromARGB(255, 19, 44, 81),
-              outlinedButtonTheme: OutlinedButtonThemeData(
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Colors.white),
-                  fixedSize: Size(200, 50),
-                ),
-              ),
-              textTheme: TextTheme(
-                bodyLarge: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                bodyMedium: TextStyle(color: Colors.white, fontSize: 20),
-                bodySmall: TextStyle(color: Colors.white),
-              ),
-            ),
-              home: OpenScreen(data: data),
-          );
-        },
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(appThemeProvider);
+
+    return MaterialApp(
+      themeMode: themeMode,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue[200],
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            side: BorderSide(color: Colors.black),
+            fixedSize: Size(200, 50),
+          ),
+        ),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyMedium: TextStyle(color: Colors.black, fontSize: 20),
+          bodySmall: TextStyle(color: Colors.black),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: const Color.fromARGB(255, 19, 44, 81),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            side: BorderSide(color: Colors.white),
+            fixedSize: Size(200, 50),
+          ),
+        ),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyMedium: TextStyle(color: Colors.white, fontSize: 20),
+          bodySmall: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: authenticationEnabled
+          ? const AuthenticationWrapper()
+          : const OpenScreen(),
+    );
+  }
+}
+
+class MainPage extends ConsumerWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const OpenScreen();
+  }
+}
+
+class AuthenticationWrapper extends ConsumerWidget {
+  const AuthenticationWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authStateAsync = ref.watch(authStateProvider);
+
+    return authStateAsync.when(
+      data: (AuthState state) {
+        return state.session == null ? const SignInPage() : const MainPage();
+      },
+      loading: () =>
+          const Scaffold(body: Center(child: CircularProgressIndicator())),
+      error: (error, __) {
+        return Scaffold(body: Center(child: Text('Error: $error')));
+      },
+    );
+  }
+}
+
+class SignInPage extends ConsumerWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign In')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => ref.read(authProvider.notifier).signInWithGoogle(),
+          child: const Text('Sign in with Google'),
+        ),
       ),
     );
   }
