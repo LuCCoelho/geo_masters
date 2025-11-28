da22929327b7fa438f9532c2beda0dd6389feb2e
Luísa Coelho <luisacoutinhocoelho@gmail.com>
2025-11-28 13:33:02 -0300

    feat: new countries data provider

---
+++ lib/providers/country_data.provider.dart
@@ -0,0 +1,14 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'supabase_provider.dart';
+
+part 'country_data.provider.g.dart';
+
+@riverpod
+Future<List<Map<String, dynamic>>> countryData(Ref ref) async {
+  final supabase = ref.watch(supabaseProvider);
+
+  // Fetch country data from Supabase
+  final response = await supabase.from('countries').select();
+
+  return List<Map<String, dynamic>>.from(response);
+}
